8fada345ba54c86abf630b5259b160da83762433
Aninam <aninamnayo@yahoo.fr>
2022-10-24 23:29:03 -0400

    Packages

---
+++ pkg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+    #Author : Utrains
+    #Date : 01-Nov-2021
+
+## ---------- script that Install some packages in Linux -----------------
+
+yum install wget -y
+if [$? -eq 0]
+then
+echo finger package install successfully!!! "
+else
+echo "Figer package was not installed please double check it and try again"
+yum install finger -y
+yum install curl -y
+yum install zip -y
+yum install vim -y
+yum install net-tools -y
+yum install  sysstat -y
+ echo "packages successfully installed"
+fi 
